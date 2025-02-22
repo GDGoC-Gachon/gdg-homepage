2598739ba7df45be155fe69006b5abc62f2e5adc
hwangjiyoung02 <hjy020714@gmail.com>
2025-02-23 00:19:44 +0900

    🛠️ refactor: JoinPeriod, register에서 admin으로 폴더 구조 재정리

--- src/main/java/com/gdg/homepage/landing/admin/repository/JoinPeriodRepository.java
+++ src/main/java/com/gdg/homepage/landing/admin/repository/JoinPeriodRepository.java
@@ -1,6 +1,6 @@
-package com.gdg.homepage.landing.register.repository;
+package com.gdg.homepage.landing.admin.repository;
 
-import com.gdg.homepage.landing.register.domain.JoinPeriod;
+import com.gdg.homepage.landing.admin.domain.JoinPeriod;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JoinPeriodRepository extends JpaRepository<JoinPeriod, Long> {
