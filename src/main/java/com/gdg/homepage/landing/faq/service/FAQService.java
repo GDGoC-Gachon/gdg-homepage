da29dbd0a6a4e7f3ccaea8828b53f64114a50ca5
hwangjiyoung02 <hjy020714@gmail.com>
2025-02-23 00:19:44 +0900

    ✨feat : FAQ 기능 구현

--- src/main/java/com/gdg/homepage/landing/faq/service/FAQService.java
+++ src/main/java/com/gdg/homepage/landing/faq/service/FAQService.java
@@ -1,4 +1,20 @@
 package com.gdg.homepage.landing.faq.service;
 
+import com.gdg.homepage.landing.faq.dto.FAQRequest;
+import com.gdg.homepage.landing.faq.dto.FAQResponse;
+
+import java.util.List;
+
 public interface FAQService {
+    // FAQ 생성
+    FAQResponse createFAQ(FAQRequest faqRequest);
+
+    // FAQ 수정
+    FAQResponse updateFAQ(Long id, FAQRequest faqRequest);
+
+    // FAQ 삭제
+    void deleteFAQ(Long id);
+
+    // FAQ 목록 조회
+    List<FAQResponse> getAllFAQs();
 }
