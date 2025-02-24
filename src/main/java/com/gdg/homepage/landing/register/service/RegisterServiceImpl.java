163850d9c268c982fe1fdb32cde8884cf5f57518
andy3400 <grayhat3400@gmail.com>
2025-02-24 13:36:12 +0900

    Feat : Add Service

--- src/main/java/com/gdg/homepage/landing/register/service/RegisterServiceImpl.java
+++ src/main/java/com/gdg/homepage/landing/register/service/RegisterServiceImpl.java
@@ -1,4 +1,36 @@
 package com.gdg.homepage.landing.register.service;
 
+import com.gdg.homepage.landing.admin.domain.JoinPeriod;
+import com.gdg.homepage.landing.register.domain.Register;
+import com.gdg.homepage.landing.register.repository.RegisterRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor(onConstructor_ = {@Autowired})
 public class RegisterServiceImpl implements RegisterService {
+
+//    @Autowired
+//    public RegisterServiceImpl(RegisterRepository registerRepository) {
+//        this.registerRepository = registerRepository;
+//    }
+
+    private final RegisterRepository registerRepository;
+
+
+    public Register CreateRegister() {
+        return registerRepository.save(new Register());
+    }
+
+    public void deleteRegister(Register register) {
+        registerRepository.delete(register);
+    }
+
+//    public List<Register> getRegisterByJoinPeriod(JoinPeriod joinPeriod) {
+//        return registerRepository.findById(joinPeriod);
+//    }
+
+
 }
