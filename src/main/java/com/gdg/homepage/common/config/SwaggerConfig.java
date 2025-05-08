530deeea5406d97f4ec8636a51884152cc8da6c0
andy3400 <grayhat3400@gmail.com>
2025-05-08 22:34:47 +0900

    feat : swagger JWT authorize 추가

--- src/main/java/com/gdg/homepage/common/config/SwaggerConfig.java
+++ src/main/java/com/gdg/homepage/common/config/SwaggerConfig.java
@@ -1,6 +1,9 @@
 package com.gdg.homepage.common.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,20 +12,26 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
 
     @Bean
-    public GroupedOpenApi boardGroupedOpenApi() {
-        return GroupedOpenApi
-                .builder()
+    public GroupedOpenApi groupedOpenApi() {
+        return GroupedOpenApi.builder()
                 .group("GDG-homepage")
-                .pathsToMatch("/**") // 모든 API 경로 포함
-                .addOpenApiCustomizer(
-                        openApi ->
-                                openApi.setInfo(
-                                        new Info()
-                                                .title("GDG api")
-                                                .description("GDG homepage를 위한 API")
-                                                .version("1.0.0")
-                                )
-                )
+                .pathsToMatch("/**")
+                .addOpenApiCustomizer(openApi -> {
+                    openApi.info(new Info()
+                            .title("GDG API")
+                            .description("GDG homepage를 위한 API")
+                            .version("1.0.0"));
+
+                    // JWT Security 추가
+                    openApi.addSecurityItem(new SecurityRequirement().addList("JWT Authorization"))
+                            .schemaRequirement("JWT Authorization",
+                                    new SecurityScheme()
+                                            .name("Authorization")
+                                            .type(SecurityScheme.Type.HTTP)
+                                            .scheme("bearer")
+                                            .bearerFormat("JWT")
+                                            .in(SecurityScheme.In.HEADER));
+                })
                 .build();
     }
 }
