7c8b350d380dd642f56469c4ca5cce3b95c13892
eedo_y <doup2001@naver.com>
2025-07-26 22:14:39 +0900

    ♻️refactor : 역할 및 학년 수정

--- src/main/java/com/gdg/homepage/landing/admin/dto/MemberListResponse.java
+++ src/main/java/com/gdg/homepage/landing/admin/dto/MemberListResponse.java
@@ -15,10 +15,10 @@ public class MemberListResponse {
     private Long memberId;
     private String email;
     private String name;
-    private String grade;
+    private int grade;
     private String studentId;
     private String phoneNumber;
-    private String role;
+    private MemberRole role;
     private boolean approved;
 
     // from
@@ -26,23 +26,23 @@ public class MemberListResponse {
 
         RegisterSnippet snippet = member.getRegister().getSnippet();
 
-        String role = String.valueOf(member.getRole());
+        MemberRole role = member.getRole();
 
         if (member.getRole().equals(MemberRole.NON_MEMBER)){
-            role = String.valueOf(member.getRegister().getRegisteredRole());
+            role = member.getRegister().getRegisteredRole().toMemberRole();
         }
+
         return MemberListResponse.builder()
                 .memberId(member.getId())
                 .email(member.getEmail())
                 .name(member.getName())
-                .grade(String.valueOf(snippet.getGrade()))
+                .grade(snippet.getGrade().getValue())
                 .studentId(snippet.getStudentId())
                 .phoneNumber(member.getPhoneNumber())
                 .role(role)
                 .approved(member.getRegister().isApproved())
                 .build();
     }
-
     // from
     public static List<MemberListResponse> from(List<Member> members) {
 
