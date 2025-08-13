ed6579e06f371716be34803ea55a0e7a1aaf62e0
eedo_y <doup2001@naver.com>
2025-08-14 03:47:24 +0900

    ♻️ refactor : 이메일 매핑 수정 - Post -> Get

--- src/main/java/com/gdg/homepage/landing/email/presentation/EmailApi.java
+++ src/main/java/com/gdg/homepage/landing/email/presentation/EmailApi.java
@@ -9,10 +9,7 @@ import com.gdg.homepage.landing.email.application.exception.NotVerifiedException
 import com.gdg.homepage.landing.email.presentation.swagger.EmailApiSpec;
 import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/email")
@@ -28,12 +25,12 @@ public class EmailApi implements EmailApiSpec {
         return ApiResponse.created("성공적으로 메일이 전송되었습니다.");
     }
 
-    @PostMapping("/verify")
+    @GetMapping("/verify")
     public ApiResponse<String> verify(@RequestBody EmailVerifyRequest request) {
         if (!emailService.verifyCode(request.getEmail(), request.getCode())){
             throw new NotVerifiedException("인증번호가 틀렸습니다.");
         }
-        return ApiResponse.created("메일이 인증되었습니다.");
+        return ApiResponse.ok("메일이 인증되었습니다.");
     }
 
 
