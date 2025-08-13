c61fea4909086845e8b85dc8d56c27129425e45b
eedo_y <doup2001@naver.com>
2025-08-14 04:28:18 +0900

    ♻️refactor : 어드민 스웨거 추가

--- src/main/java/com/gdg/homepage/landing/admin/application/dto/request/MemberUpgradeRequest.java
+++ src/main/java/com/gdg/homepage/landing/admin/application/dto/request/MemberUpgradeRequest.java
@@ -1,6 +1,7 @@
 package com.gdg.homepage.landing.admin.application.dto.request;
 
 import com.gdg.homepage.landing.member.domain.entity.MemberRole;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import jakarta.validation.constraints.Min;
@@ -9,10 +10,12 @@ import jakarta.validation.constraints.NotNull;
 @Data
 public class MemberUpgradeRequest {
 
+    @Schema(example = "1",description = "관리자 ID")
     private Long adminId;
 
     @NotNull(message = "회원 ID는 필수입니다.")
     @Min(value = 1, message = "회원 ID는 1 이상이어야 합니다.")
+    @Schema(example = "2",description = "승인할 유저 ID")
     private Long memberId;
 
     @NotNull(message = "변경할 역할은 필수입니다.")
